e00cffe8eb28b44c46bcf7f8a84e2f4aea399c20
Matthew Kanwisher <matt@kanwisher.com>
2014-06-13 22:23:35 -0400

    got some ui stuff showing in playground

---
+++ board_logic.playground/section-1.swift
@@ -0,0 +1,41 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+import SpriteKit
+//import XCPlayground
+
+var grid  = String[]()
+var height = 20
+var width = 20
+
+//grid[0] = "abcdefghi"
+//grid[1] = "bbbcd"
+//grid[2] = "123dsafdsa"
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+view.layer.cornerRadius = 15
+view.backgroundColor = UIColor.redColor()
+view
+
+let s = CGSizeMake(100, 100)
+
+/*
+var rectSprite = SKSpriteNode.spriteNodeWithColor(SKColor: SKColor.blackColor(), size:s)
+rectSprite.position = CGPointZero;
+rectSprite
+*/
+
+
+
+/*
+var bundle = NSBundle.mainBundle()
+var path = bundle.resourcePath
+
+println(path)
+bundle
+
+
+var sample = UIImage(named: "swift.jpeg")
+sample
+
+*/
